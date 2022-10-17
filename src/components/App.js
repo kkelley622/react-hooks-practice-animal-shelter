40e3e6de95193d31d407e0714aa5286c22c372ca
Kevin Kelley <kkelley622@gmail.com>
2022-10-17 17:53:26 -0500

    commit tree 828fb22ba0b04103e11855b32580acb3b8fb9620

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,35 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  let url = "http://localhost:3001/pets"
+
+  const handleTypeChange = (type) => {
+    console.log(type)
+    setFilters({ type: type })
+  }
+
+  const handleTypeClick = () => {
+    if(filters.type !== "all") {
+      url += `?type=${filters.type}`
+    }
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setPets(data)
+      })
+  }
+
+  const adoptPet = (id) => {
+    
+    const adoptedPets = pets.map((pet) => {
+      return pet.id === id ? {...pet, isAdopted: true} : pet});
+    setPets(adoptedPets)
+    
+  } 
+
+
+
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +44,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters handleTypeChange={handleTypeChange} handleTypeClick={handleTypeClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={adoptPet}/>
           </div>
         </div>
       </div>
